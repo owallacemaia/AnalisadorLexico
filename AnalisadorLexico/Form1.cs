cf7997445f85e4a481291ae77617e44ddef5e7cf
Wallace Maia <wallacemaiag@outlook.com.br>
2021-03-09 22:21:39 -0300

    Criação da interface

--- AnalisadorLexico/Form1.cs
+++ AnalisadorLexico/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AnalisadorLexico
 {
-    public partial class Form1 : Form
+    public partial class frmPrincipal : MaterialSkin.Controls.MaterialForm
     {
-        public Form1()
+        public frmPrincipal()
         {
             InitializeComponent();
         }
